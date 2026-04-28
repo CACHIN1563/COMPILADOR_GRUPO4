2d7a718b80ab5fd3c1b449d2380636499bbd0c8d
studentHub777 <135550751+studentHub777@users.noreply.github.com>
2026-04-27 23:12:10 -0600

    Ejecución de gramática
    Se agregan tipos explícitos, funciones y ciclos for

---
+++ 02_Fase2_Semantico_Interprete/GramaticaListener.py
@@ -0,0 +1,399 @@
+# Generated from Gramatica.g4 by ANTLR 4.13.1
+from antlr4 import *
+if "." in __name__:
+    from .GramaticaParser import GramaticaParser
+else:
+    from GramaticaParser import GramaticaParser
+
+# This class defines a complete listener for a parse tree produced by GramaticaParser.
+class GramaticaListener(ParseTreeListener):
+
+    # Enter a parse tree produced by GramaticaParser#ProgramG4.
+    def enterProgramG4(self, ctx:GramaticaParser.ProgramG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ProgramG4.
+    def exitProgramG4(self, ctx:GramaticaParser.ProgramG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#DeclVariableG4.
+    def enterDeclVariableG4(self, ctx:GramaticaParser.DeclVariableG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#DeclVariableG4.
+    def exitDeclVariableG4(self, ctx:GramaticaParser.DeclVariableG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#DeclFuncionG4.
+    def enterDeclFuncionG4(self, ctx:GramaticaParser.DeclFuncionG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#DeclFuncionG4.
+    def exitDeclFuncionG4(self, ctx:GramaticaParser.DeclFuncionG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#DeclInstruccionG4.
+    def enterDeclInstruccionG4(self, ctx:GramaticaParser.DeclInstruccionG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#DeclInstruccionG4.
+    def exitDeclInstruccionG4(self, ctx:GramaticaParser.DeclInstruccionG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#VarDeclarationG4.
+    def enterVarDeclarationG4(self, ctx:GramaticaParser.VarDeclarationG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#VarDeclarationG4.
+    def exitVarDeclarationG4(self, ctx:GramaticaParser.VarDeclarationG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#FuncDeclarationG4.
+    def enterFuncDeclarationG4(self, ctx:GramaticaParser.FuncDeclarationG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#FuncDeclarationG4.
+    def exitFuncDeclarationG4(self, ctx:GramaticaParser.FuncDeclarationG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#params.
+    def enterParams(self, ctx:GramaticaParser.ParamsContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#params.
+    def exitParams(self, ctx:GramaticaParser.ParamsContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#param.
+    def enterParam(self, ctx:GramaticaParser.ParamContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#param.
+    def exitParam(self, ctx:GramaticaParser.ParamContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#TypeIntG4.
+    def enterTypeIntG4(self, ctx:GramaticaParser.TypeIntG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#TypeIntG4.
+    def exitTypeIntG4(self, ctx:GramaticaParser.TypeIntG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#TypeFloatG4.
+    def enterTypeFloatG4(self, ctx:GramaticaParser.TypeFloatG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#TypeFloatG4.
+    def exitTypeFloatG4(self, ctx:GramaticaParser.TypeFloatG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#TypeStringG4.
+    def enterTypeStringG4(self, ctx:GramaticaParser.TypeStringG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#TypeStringG4.
+    def exitTypeStringG4(self, ctx:GramaticaParser.TypeStringG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#TypeBoolG4.
+    def enterTypeBoolG4(self, ctx:GramaticaParser.TypeBoolG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#TypeBoolG4.
+    def exitTypeBoolG4(self, ctx:GramaticaParser.TypeBoolG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#TypeVoidG4.
+    def enterTypeVoidG4(self, ctx:GramaticaParser.TypeVoidG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#TypeVoidG4.
+    def exitTypeVoidG4(self, ctx:GramaticaParser.TypeVoidG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#AsignacionVariableG4.
+    def enterAsignacionVariableG4(self, ctx:GramaticaParser.AsignacionVariableG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#AsignacionVariableG4.
+    def exitAsignacionVariableG4(self, ctx:GramaticaParser.AsignacionVariableG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaIfElseG4.
+    def enterSentenciaIfElseG4(self, ctx:GramaticaParser.SentenciaIfElseG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaIfElseG4.
+    def exitSentenciaIfElseG4(self, ctx:GramaticaParser.SentenciaIfElseG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaMientrasG4.
+    def enterSentenciaMientrasG4(self, ctx:GramaticaParser.SentenciaMientrasG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaMientrasG4.
+    def exitSentenciaMientrasG4(self, ctx:GramaticaParser.SentenciaMientrasG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaForG4.
+    def enterSentenciaForG4(self, ctx:GramaticaParser.SentenciaForG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaForG4.
+    def exitSentenciaForG4(self, ctx:GramaticaParser.SentenciaForG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaImprimirG4.
+    def enterSentenciaImprimirG4(self, ctx:GramaticaParser.SentenciaImprimirG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaImprimirG4.
+    def exitSentenciaImprimirG4(self, ctx:GramaticaParser.SentenciaImprimirG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaReturnG4.
+    def enterSentenciaReturnG4(self, ctx:GramaticaParser.SentenciaReturnG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaReturnG4.
+    def exitSentenciaReturnG4(self, ctx:GramaticaParser.SentenciaReturnG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#StatBloqueG4.
+    def enterStatBloqueG4(self, ctx:GramaticaParser.StatBloqueG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#StatBloqueG4.
+    def exitStatBloqueG4(self, ctx:GramaticaParser.StatBloqueG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#SentenciaExpresionG4.
+    def enterSentenciaExpresionG4(self, ctx:GramaticaParser.SentenciaExpresionG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#SentenciaExpresionG4.
+    def exitSentenciaExpresionG4(self, ctx:GramaticaParser.SentenciaExpresionG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#bloque.
+    def enterBloque(self, ctx:GramaticaParser.BloqueContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#bloque.
+    def exitBloque(self, ctx:GramaticaParser.BloqueContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#AsignacionCoreG4.
+    def enterAsignacionCoreG4(self, ctx:GramaticaParser.AsignacionCoreG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#AsignacionCoreG4.
+    def exitAsignacionCoreG4(self, ctx:GramaticaParser.AsignacionCoreG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#condicional.
+    def enterCondicional(self, ctx:GramaticaParser.CondicionalContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#condicional.
+    def exitCondicional(self, ctx:GramaticaParser.CondicionalContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#bucle_mientras.
+    def enterBucle_mientras(self, ctx:GramaticaParser.Bucle_mientrasContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#bucle_mientras.
+    def exitBucle_mientras(self, ctx:GramaticaParser.Bucle_mientrasContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#bucle_for.
+    def enterBucle_for(self, ctx:GramaticaParser.Bucle_forContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#bucle_for.
+    def exitBucle_for(self, ctx:GramaticaParser.Bucle_forContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#impresion.
+    def enterImpresion(self, ctx:GramaticaParser.ImpresionContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#impresion.
+    def exitImpresion(self, ctx:GramaticaParser.ImpresionContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#sentencia_return.
+    def enterSentencia_return(self, ctx:GramaticaParser.Sentencia_returnContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#sentencia_return.
+    def exitSentencia_return(self, ctx:GramaticaParser.Sentencia_returnContext):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprRelacionalCompG4.
+    def enterExprRelacionalCompG4(self, ctx:GramaticaParser.ExprRelacionalCompG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprRelacionalCompG4.
+    def exitExprRelacionalCompG4(self, ctx:GramaticaParser.ExprRelacionalCompG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprPotenciaG4.
+    def enterExprPotenciaG4(self, ctx:GramaticaParser.ExprPotenciaG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprPotenciaG4.
+    def exitExprPotenciaG4(self, ctx:GramaticaParser.ExprPotenciaG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLogicaOrG4.
+    def enterExprLogicaOrG4(self, ctx:GramaticaParser.ExprLogicaOrG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLogicaOrG4.
+    def exitExprLogicaOrG4(self, ctx:GramaticaParser.ExprLogicaOrG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprAritmeticaMultDivG4.
+    def enterExprAritmeticaMultDivG4(self, ctx:GramaticaParser.ExprAritmeticaMultDivG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprAritmeticaMultDivG4.
+    def exitExprAritmeticaMultDivG4(self, ctx:GramaticaParser.ExprAritmeticaMultDivG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprAritmeticaSumaResG4.
+    def enterExprAritmeticaSumaResG4(self, ctx:GramaticaParser.ExprAritmeticaSumaResG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprAritmeticaSumaResG4.
+    def exitExprAritmeticaSumaResG4(self, ctx:GramaticaParser.ExprAritmeticaSumaResG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLlamadaFuncionG4.
+    def enterExprLlamadaFuncionG4(self, ctx:GramaticaParser.ExprLlamadaFuncionG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLlamadaFuncionG4.
+    def exitExprLlamadaFuncionG4(self, ctx:GramaticaParser.ExprLlamadaFuncionG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLogicaAndG4.
+    def enterExprLogicaAndG4(self, ctx:GramaticaParser.ExprLogicaAndG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLogicaAndG4.
+    def exitExprLogicaAndG4(self, ctx:GramaticaParser.ExprLogicaAndG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLogicaNotG4.
+    def enterExprLogicaNotG4(self, ctx:GramaticaParser.ExprLogicaNotG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLogicaNotG4.
+    def exitExprLogicaNotG4(self, ctx:GramaticaParser.ExprLogicaNotG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprAgrupacionG4.
+    def enterExprAgrupacionG4(self, ctx:GramaticaParser.ExprAgrupacionG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprAgrupacionG4.
+    def exitExprAgrupacionG4(self, ctx:GramaticaParser.ExprAgrupacionG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLiteralBoolG4.
+    def enterExprLiteralBoolG4(self, ctx:GramaticaParser.ExprLiteralBoolG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLiteralBoolG4.
+    def exitExprLiteralBoolG4(self, ctx:GramaticaParser.ExprLiteralBoolG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLiteralCadenaG4.
+    def enterExprLiteralCadenaG4(self, ctx:GramaticaParser.ExprLiteralCadenaG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLiteralCadenaG4.
+    def exitExprLiteralCadenaG4(self, ctx:GramaticaParser.ExprLiteralCadenaG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprRelacionalIgualdadG4.
+    def enterExprRelacionalIgualdadG4(self, ctx:GramaticaParser.ExprRelacionalIgualdadG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprRelacionalIgualdadG4.
+    def exitExprRelacionalIgualdadG4(self, ctx:GramaticaParser.ExprRelacionalIgualdadG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprReferenciaVariableG4.
+    def enterExprReferenciaVariableG4(self, ctx:GramaticaParser.ExprReferenciaVariableG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprReferenciaVariableG4.
+    def exitExprReferenciaVariableG4(self, ctx:GramaticaParser.ExprReferenciaVariableG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#ExprLiteralNumericaG4.
+    def enterExprLiteralNumericaG4(self, ctx:GramaticaParser.ExprLiteralNumericaG4Context):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#ExprLiteralNumericaG4.
+    def exitExprLiteralNumericaG4(self, ctx:GramaticaParser.ExprLiteralNumericaG4Context):
+        pass
+
+
+    # Enter a parse tree produced by GramaticaParser#args.
+    def enterArgs(self, ctx:GramaticaParser.ArgsContext):
+        pass
+
+    # Exit a parse tree produced by GramaticaParser#args.
+    def exitArgs(self, ctx:GramaticaParser.ArgsContext):
+        pass
+
+
+
+del GramaticaParser
